5509797fb17e5a90e2c1a8aa18f8cb7e902b49fb
Thomas Shivers <tcshivers1970@yahoo.com>
2015-10-29 17:45:56 +0000

    master

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,33 +1,23 @@
 require_relative 'entry'
  
- class AddressBook
+class AddressBook
     attr_accessor :entries
     
-    def initialize
-        @entries = []
-    end
-    
-    def remove_entry(name, phone_number, email)
-        delete_entry = nil
-        @entries.each do |entry|
-            if name = entry.name && phone = entry.phone_number && email = entry.email_address
-                delete_entry = entry
-            end
-        end
-        @entries.delete(delete_entry)
-    end
+  def initialize
+   @entries = []
+  end
    
-    def add_entry(name, phone_number, email)
- # #9
-     index = 0
-     @entries.each do |entry|
- # #10
-     if name < entry.name
-        break
-     end
-       index += 1
+  def add_entry(name, phone_number, email)
+ 
+   index = 0
+   @entries.each do |entry|
+ 
+    if name < entry.name
+     break
     end
- # #11
-     @entries.insert(index, Entry.new(name, phone_number, email))
+     index += 1
    end
- end
+      @entries.insert(index, Entry.new(name, phone_number, email))
+  end
+end
+    
--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -1,55 +1,44 @@
- require_relative '../models/address_book'
-
- RSpec.describe AddressBook do  
-    describe "attributes" do
-     it "should respond to entries" do
-       book = AddressBook.new
-       expect(book).to respond_to(:entries)
-     end
-
-     it "should initialize entries as an array" do
-       book = AddressBook.new
-       expect(book.entries).to be_a(Array)
-     end
+require_relative '../models/address_book'
  
-     it "should initialize entries as empty" do
-       book = AddressBook.new
-       expect(book.entries.size).to eq(0)
-     end
+RSpec.describe AddressBook do  
+   
+   describe "attributes" do
+    it "should respond to entries" do
+     book = AddressBook.new
+     
+     expect(book).to respond_to(:entries)
     end
     
-    context "remove_entry" do
-    it "removes an entry using name, phone_number, and email_address" do
-     book = Addressbook.new
-     book.add_entry("Michael Faraday", "010.210.5181", "michael.faraday@faraday.com")
+    it "should initialize entries as an array" do
+     book = AddressBook.new
      
-     name = "Ada Lovelace"
-     phone_number = "010.012.1815"
-     email_address = "augusta.king@lovelace.com"
-     book.add_entry(name, phone_number, email_address)
-     
-     expect(new_entry.name).to eq(2)
-     book.remove_entry(name, phone_number, email_address)
-     expect(book.entries.size).to eq(1)
-     expect(book.entries.first.name). to eq("Michael Faraday")
+     expect(book.entries).to be_a(Array)
     end
+ 
+    it "should initialize entries as empty" do
+     book = AddressBook.new
+     
+     expect(book.entries.size).to eq(0)
+    end
+   end
 
-    describe "#add_entry" do
-     it "adds only one entry to the address book" do
-       book = AddressBook.new
-       book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+   describe "#add_entry" do
+    it "adds only one entry to the address book" do
+     book = AddressBook.new
+     book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
  
-       expect(book.entries.size).to eq(1)
-     end
- 
-     it "adds the correct information to entries" do
-       book = AddressBook.new
-       book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
-       new_entry = book.entries[0]
- 
-       expect(new_entry.name).to eq('Ada Lovelace')
-       expect(new_entry.phone_number).to eq('010.012.1815')
-       expect(new_entry.email).to eq('augusta.king@lovelace.com')
-     end
+     expect(book.entries.size).to eq(1)
     end
+ 
+    it "adds the correct information to entries" do
+     book = AddressBook.new
+     book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+     new_entry = book.entries[0]
+ 
+     expect(new_entry.name).to eq('Ada Lovelace')
+     expect(new_entry.phone_number).to eq('010.012.1815')
+     expect(new_entry.email).to eq('augusta.king@lovelace.com')
+    end
+   end
+   
  end
