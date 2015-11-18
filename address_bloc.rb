dd4a8f1dbcb2d1c411aff23e4ab53a9a70460631
Thomas Shivers <tcshivers1970@yahoo.com>
2015-11-18 03:34:29 +0000

    Address Bloc

--- address_bloc.rb
+++ address_bloc.rb
@@ -8,12 +8,3 @@ require_relative 'controllers/menu_controller'
  # #6
  menu.main_menu
  
- def import_from_csv(file_name)
-# Implementation goes here
-   csv_text = File.read(file_name)
-   csv = csv.parse(csv_text, headers: true, skip_blanks: true)
-   csv.each do |row|
-    row_hash = row.to_hash
-    add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
-   end
- end
