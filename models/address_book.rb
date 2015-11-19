01963a65f1d25ecec5e5007b7fc4e5d10d23b78a
Thomas Shivers <tcshivers1970@yahoo.com>
2015-11-19 21:56:55 +0000

    Models Address Book

--- models/address_book.rb
+++ models/address_book.rb
@@ -24,7 +24,7 @@ class AddressBook
   def import_from_csv(file_name)
 # Implementation goes here
    csv_text = File.read(file_name)
-   csv = csv.parse(csv_text, headers: true, skip_blanks: true)
+   csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
    csv.each do |row|
     row_hash = row.to_hash
     add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
@@ -43,6 +43,23 @@ class AddressBook
   end
   
   def binary_search(name)
+ # #1
+     lower = 0
+     upper = entries.length - 1
+# #2
+     while lower <= upper
+# #3
+       mid = (lower + upper) / 2
+       mid_name = entries[mid].name
+# #4
+       if name == mid_name
+         return entries[mid]
+       elsif name < mid_name
+         upper = mid - 1
+       elsif name > mid_name
+         lower = mid + 1
+       end
+     end
    return nil
   end
   
