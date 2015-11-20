bdf04d63c2e63068031b797d2348eb82fc46a31a
Thomas Shivers <tcshivers1970@yahoo.com>
2015-11-20 21:40:51 +0000

    Spec Address Book

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -10,7 +10,18 @@ RSpec.describe AddressBook do
     expect(entry.phone_number).to eq expected_number
     expect(entry.email).to eq expected_email
   end
-
+  
+  context "#detonate" do
+   it "should delete all entries" do
+    book.add_entry("Ada Lovelace", "010.012.1815", "augusta.king@lovelace.com")
+    book.add_entry("Ada Lovelace", "010.012.1815", "augusta.king@lovelace.com")
+    book.add_entry("Ada Lovelace", "010.012.1815", "augusta.king@lovelace.com")
+   
+    book.detonate
+    expect(book.entries.size).to eq 0
+   end
+  end
+  
    describe "attributes" do
     it "should respond to entries" do
      #book = AddressBook.new
